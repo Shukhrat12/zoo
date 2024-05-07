25303dee72f1c14783ee724403c292b4e302fc1b
Shukhfayz <shukhfayz@gmail.com>
2024-05-06 23:12:13 -0400

    feat: create post api request

--- src/dog/dog.service.ts
+++ src/dog/dog.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DogService {
-    public getHello() {
+    public getHello(): string {
         return `WOW-WOW TO HOME PAGE`
     }
 
-    public introduce() {
+    public introduce(): string {
         return "My name is Jerry!"
+    }
+
+    public modifyDetail(): string {
+        return 'successfully modified'
     }
 }
